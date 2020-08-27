ee6016497d62097e73a764edf53ed6a99ed24d69
AngelValle0396 <66800307+AngelValle0396@users.noreply.github.com>
2020-08-26 23:55:06 -0500

    nuevo
    Commit

---
+++ app/src/main/java/com/example/tareagps/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tareagps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
